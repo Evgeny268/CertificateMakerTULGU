7db8a4cbff2fbab8ec943947a5ee0f3492f72b5f
Evgeny268 <evgepishcin@gmail.com>
2020-04-03 15:46:55 +0300

    add WordWorker and ExcelWorker

---
+++ core/office/ExcelWorker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+/// <summary>
+/// Класс для работы с документами Microsoft Excel
+/// </summary>
+namespace CertificateMaker.core.office
+{
+    class ExcelWorker
+    {
+        /// <summary>
+        /// Чтение из Excel необходимых ячеек
+        /// </summary>
+        /// <param name="filepath">путь до файла на диске</param>
+        /// <param name="startRow">строка, с которое следует начать чтение</param>
+        /// <param name="endRow">строка, на которое следует завершить чтение</param>
+        /// <param name="cellsNum">номера столбцов, которые следует прочитать</param>
+        /// <returns></returns>
+        public static List<string[]> ReadCells(string filepath, int startRow, int endRow, int[] cellsNum)
+        {
+            List<string[]> returnCellsVaule = new List<string[]>();
+            Excel.Application app = null;
+            try
+            {
+                app = new Excel.Application();
+                Excel.Workbook workbook = app.Workbooks.Open(filepath, 0, true);
+                Excel._Worksheet worksheet = workbook.Sheets[1];
+                Excel.Range range = worksheet.UsedRange;
+
+                for (int i = startRow; i <= endRow; i++)
+                {
+                    string[] row = new string[cellsNum.Length];
+                    for (int j = 0; j < cellsNum.Length; j++)
+                    {
+                        if (range.Cells[i, cellsNum[j]] != null && range.Cells[i, cellsNum[j]].Value2 != null)
+                        {
+                            row[j] = range.Cells[i, cellsNum[j]].Value2.ToString();
+                        }
+                        else
+                        {
+                            row[j] = "NULL";
+                        }
+                    }
+                    returnCellsVaule.Add(row);
+                }
+                workbook.Close();
+            }
+            finally
+            {
+                try
+                {
+                    app.Quit();
+                }
+                catch (Exception) { }
+            }
+            return returnCellsVaule;
+        }
+    }
+}
---
+++ core/office/WordWorker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+/// <summary>
+/// Класс для работы с документами Microsoft Word
+/// </summary>
+namespace CertificateMaker.core.office
+{
+    class WordWorker
+    {
+        /// <summary>
+        /// Заменяет выбранный текст в документе Word на указанный
+        /// </summary>
+        /// <param name="filepath">путь до документа на диске</param>
+        /// <param name="wordsForReplace">словарь замены слов, ключ - слово, которое следует заменить, значение - слово, на которое нужно заменить</param>
+        public static void ReplaceText(string filepath, Dictionary<string, string> wordsForReplace)
+        {
+            Word.Application app = null;
+            try
+            {
+                app = new Word.Application();
+                Word.Document doc = app.Documents.Open(filepath);
+                doc.Activate();
+                foreach (KeyValuePair<string, string> keyValue in wordsForReplace)
+                {
+                    FindAndReplace(app, keyValue.Key, keyValue.Value);
+                }
+                doc.Save();
+                doc.Close();
+            }
+            finally
+            {
+                try
+                {
+                    app.Quit();
+                }
+                catch (Exception) { }
+            }
+        }
+
+        protected static void FindAndReplace(Microsoft.Office.Interop.Word.Application doc, object findText, object replaceWithText)
+        {
+            //options
+            object matchCase = false;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+            //execute find and replace
+            doc.Selection.Find.Execute(ref findText, ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundsLike, ref matchAllWordForms, ref forward, ref wrap, ref format, ref replaceWithText, ref replace,
+                ref matchKashida, ref matchDiacritics, ref matchAlefHamza, ref matchControl);
+        }
+    }
+}
