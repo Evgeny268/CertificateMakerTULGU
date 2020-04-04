0fdb3fa37611f2da4f10f03e7188b4b5927cc314
Evgeny268 <evgepishcin@gmail.com>
2020-04-04 19:29:23 +0300

    add presets

---
+++ core/presets/Preset.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertificateMaker.core.presets
+{
+    /// <summary>
+    /// Пресет для программы
+    /// </summary>
+    class Preset
+    {
+        private string templatePath { get; set; } = null;
+        private string excelPath { get; set; } = null;
+        private Nullable<int> startRowImport { get; set; } = null;
+        private Nullable<int> endRowImport { get; set; } = null;
+        List<Table> rows { get; set; } = null;
+
+        /// <summary>
+        /// Конструктор для пресета
+        /// </summary>
+        /// <param name="templatePath">Путь до файла с шаблоном</param>
+        /// <param name="excelPath">Путь до файла excel</param>
+        /// <param name="startRowImport">Номер строки, с которой начать импорт</param>
+        /// <param name="endRowImport">Номер строки, на которой завершить импорт</param>
+        /// <param name="rows">Лист с настройками для каждого тега из шаблона</param>
+        public Preset(string templatePath, string excelPath, int? startRowImport, int? endRowImport, List<Table> rows)
+        {
+            this.templatePath = templatePath;
+            this.excelPath = excelPath;
+            this.startRowImport = startRowImport;
+            this.endRowImport = endRowImport;
+            this.rows = rows;
+        }
+    }
+}
---
+++ core/presets/Table.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertificateMaker.core.presets
+{
+    /// <summary>
+    /// Пресеты для каждого тега из шаблона
+    /// </summary>
+    class Table
+    {
+        private string templateField { get; set; } = null;
+        private TemplateType type { get; set; } = TemplateType.excel;
+        private Nullable<int> value { get; set; } = null;
+
+        /// <summary>
+        /// Конструктор для тега из шаблона
+        /// </summary>
+        /// <param name="templateField">название тега</param>
+        /// <param name="type">тип тега</param>
+        /// <param name="value">значение</param>
+        public Table(string templateField, TemplateType type, int? value)
+        {
+            this.templateField = templateField;
+            this.type = type;
+            this.value = value;
+        }
+    }
+
+    public enum TemplateType
+    {
+        excel,
+        generate
+    }
+}
