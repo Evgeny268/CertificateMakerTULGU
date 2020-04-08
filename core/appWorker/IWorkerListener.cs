8aad1335d7a71b1f4599e5e2bd2223c3f3f47fb0
Evgeny268 <evgepishcin@gmail.com>
2020-04-08 18:57:19 +0300

    add comments to interface WorkerListener

--- core/appWorker/IWorkerListener.cs
+++ core/appWorker/IWorkerListener.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace CertificateMaker.core.appWorker
 {
+    /// <summary>
+    /// Слушатель для отслеживания прогресса
+    /// </summary>
     interface IWorkerListener
     {
+        /// <summary>
+        /// Вызывается в момент выполнения очередного шага
+        /// </summary>
+        /// <param name="stage">Текущий этап</param>
+        /// <param name="current">Текущее значение</param>
+        /// <param name="all">Всего необходимо выполнить</param>
         void WorkStatus(WorkStage stage, int current, int all);
     }
 
     public enum WorkStage
     {
-        READ_FROM_EXCEL,
-        CREATE_DOC,
-        MERGE_DOC,
-        DELETE_TEMP_FILES
+        READ_FROM_EXCEL, //Чтение из excel
+        CREATE_DOC, //Создание документа word, тут применяется отслеживание прогресса (int current, int all)
+        MERGE_DOC, //Объединение документов
+        DELETE_TEMP_FILES //Удаление временной директории
     }
 }
