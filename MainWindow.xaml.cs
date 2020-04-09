0e5f45bdccb928a0545745381df6b0e081079582
theDAREK497 <ilion9871@gmail.com>
2020-04-09 19:24:02 +0300

    wip window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,12 +27,65 @@ namespace CertificateMaker
 
         private void Add_Button_Click(object sender, RoutedEventArgs e)
         {
-            //var data = new User { TagName = "1", Value = "2" };
+            //var data = new user { tagname = "1", value = "2" };
 
-            //dataGrid.Items.Add(data);
-            core.test.Test tester = new core.test.Test();
+            //datagrid.items.add(data);
+        }
 
-            tester.Tester();
+        private void btnWordLoad_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".DOC .DOCX .DOCM"; // Default file extension
+            dlg.Filter = "MS Word Документ|*.DOC;*.DOCX;*.DOCM"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                WordFileName.Content = dlg.FileName;
+            }
+        }
+
+        private void btnExcelLoad_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".xls"; // Default file extension
+            dlg.Filter = "Excel таблица|*.xls;*.xlsx"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                ExcelFileName.Content = dlg.FileName;
+            }
+        }
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(IsGood);
+        }
+        private void OnPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            var stringData = (string)e.DataObject.GetData(typeof(string));
+            if (stringData == null || !stringData.All(IsGood))
+                e.CancelCommand();
+        }
+
+        bool IsGood(char c)
+        {
+            if (c >= '0' && c <= '9')
+                return true;
+            return false;
         }
     }
     public class User
